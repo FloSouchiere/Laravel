3f3af0052c9f42e653d025e26b52c12a12875f46
MBP <onin@MacBook-Pro.local>
2020-11-13 08:37:43 +0100

    Push des migrations, connexion BDD KO

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,17 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('firstname');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email', 80)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('phone');
+            $table->string('address_line_1');
+            $table->string('address_line_2');
+            $table->integer('zipcode');
+            $table->string('city');
+            $table->boolean('is_admin');
             $table->rememberToken();
             $table->timestamps();
         });
